63b7363174e586f2d9ef1bdc07ad0e2112073684
Alex Klimenkov <alex.klimenkov89@gmail.com>
2012-09-26 14:26:35 +0300

    change visible for variables

--- lib/autotest-users.rb
+++ lib/autotest-users.rb
@@ -15,46 +15,46 @@ module Autotest
     def create_user(name)
       require "randexp"
 
-      @users ||= Hash.new
-      @users[name] ||= Hash.new
+      $users ||= Hash.new
+      $users[name] ||= Hash.new
 
       first_name = /[:first_name:]/.gen
       last_name = /[:last_name:]/.gen
 
-      @users[name]['first_name'] = first_name
-      @users[name]['last_name'] = last_name
+      $users[name]['first_name'] = first_name
+      $users[name]['last_name'] = last_name
       email = Autotest.email.split('@')
-      @users[name]['email'] = "%s+%s%s@%s" % [email[0], first_name.downcase, last_name.downcase, email[1]]
-      @users[name]['password'] = Autotest.password
+      $users[name]['email'] = "%s+%s%s@%s" % [email[0], first_name.downcase, last_name.downcase, email[1]]
+      $users[name]['password'] = Autotest.password
     end
 
     def get_user(name)
-      @users[name]
+      $users[name]
     end
 
     def set_user_data(name, type, data)
-      @users[name][type] = data
+      $users[name][type] = data
     end
 
     def get_user_data(name, type)
-      @users[name][type]
+      $users[name][type]
     end
 
     def set_current(name)
-      @current ||= Hash.new
+      $current ||= Hash.new
       if ((name != 'anonymous') or (name != 'anonim'))
-	@current['first_name'] = @users[name]['first_name']
-	@current['email'] = @users[name]['email']
-	@current['password'] = @users[name]['password']
+	$current['first_name'] = @users[name]['first_name']
+	$current['email'] = @users[name]['email']
+	$current['password'] = @users[name]['password']
       else
-	@current['first_name'] = 'anonymous'
-	@current['email'] = 'anonymous'
-	@current['password'] = 'anonymous'
+	$current['first_name'] = 'anonymous'
+	$current['email'] = 'anonymous'
+	$current['password'] = 'anonymous'
       end
     end
 
     def get_current(type)
-      @current[type]
+      $current[type]
     end
 
   end
