2ca6d0fe29aaa39ac115a5bdd6777ff7b0389983
Alex Klimenkov <alex.klimenkov89@gmail.com>
2012-09-26 13:29:18 +0300

    add methods for users module

--- autotest-users.gemspec
+++ autotest-users.gemspec
@@ -4,9 +4,9 @@ require File.expand_path('../lib/autotest-users/version', __FILE__)
 Gem::Specification.new do |gem|
   gem.authors       = ["Alex Klimenkov"]
   gem.email         = ["alex.klimenkov89@gmail.com"]
-  gem.description   = %q{A gem description}
-  gem.summary       = %q{A gem summary}
-  gem.homepage      = ""
+  gem.description   = %q{Gem for provide user for autotest}
+  gem.summary       = %q{Gem create, update, save some params for users. All users are saved in the Hash and will available, when automated test is running. Users are not saved into database. Today it is not needed.}
+  gem.homepage      = "https://github.com/AlexKlim/autotest-users"
 
   gem.files         = `git ls-files`.split($\)
   gem.executables   = gem.files.grep(%r{^bin/}).map{ |f| File.basename(f) }
@@ -14,4 +14,6 @@ Gem::Specification.new do |gem|
   gem.name          = "autotest-users"
   gem.require_paths = ["lib"]
   gem.version       = Autotest::Users::VERSION
+  gem.add_dependency("randexp")
+
 end
--- lib/autotest-users.rb
+++ lib/autotest-users.rb
@@ -1,10 +1,66 @@
 require "autotest-users/version"
 
 module Autotest
-  module Users
-    # Your code goes here...
-    def hello
-      p "Hello from gem !"
+
+  class << self
+    attr_accessor :email, :password
+
+    def configure
+      yield self
     end
   end
+
+  module Users
+
+    def create_user(name)
+      require "randexp"
+
+      @users ||= Hash.new
+      @users[name] ||= Hash.new
+
+      first_name = /[:first_name:]/.gen
+      last_name = /[:last_name:]/.gen
+
+      @users[name]['first_name'] = first_name
+      @users[name]['last_name'] = last_name
+      email = Autotest.email.split('@')
+      @users[name]['email'] = "%s+%s%s@%s" % [email[0], first_name.downcase, last_name.downcase, email[1]]
+      @users[name]['password'] = Autotest.password
+    end
+
+    def get_user(name)
+      @users[name]
+    end
+
+    def set_user_data(name, type, data)
+      @users[name][type] = data
+    end
+
+    def get_user_data(name, type)
+      @users[name][type]
+    end
+
+    def set_current(name)
+      @current ||= Hash.new
+      if ((name != 'anonymous') or (name != 'anonim'))
+	@current['first_name'] = @users[name]['first_name']
+	@current['email'] = @users[name]['email']
+	@current['password'] = @users[name]['password']
+      else
+	@current['first_name'] = 'anonymous'
+	@current['email'] = 'anonymous'
+	@current['password'] = 'anonymous'
+      end
+    end
+
+    def get_current(type)
+      @current[type]
+    end
+
+  end
+end
+
+Autotest.configure do |config|
+  config.email = 'email@example.com'
+  config.password = 'password'
 end
