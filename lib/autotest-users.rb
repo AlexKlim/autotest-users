ba899e0f213e4ccf4bd4a443ec88ada0ebe16ea1
Andrey Botalov <botalov.andrey@gmail.com>
2013-01-16 21:43:05 +0300

    cleanup
    Not tested

--- lib/autotest-users.rb
+++ lib/autotest-users.rb
@@ -1,4 +1,4 @@
-require "autotest-users/version"
+require 'autotest-users/version'
 
 module Autotest
 
@@ -10,80 +10,84 @@ module Autotest
     end
   end
 
+  class User
+    @data = {}
+
+    def [](key)
+      key = key.to_sym
+      return "#{@data[:first_name]} #{@data[:last_name]}" if key == :name
+      @data[key]
+    end
+
+    def []=(key, value)
+      @data[key.to_sym] = value
+    end
+  end
+
   module Users
+    @@users = {}
 
-    def create_user(name)
-      require "randexp"
+    def create_user(short_name)
+      require 'randexp'
+      short_name = short_name.to_s
+      user = @@users[short_name] = User.new
 
-      $users ||= Hash.new
-      $users[name] ||= Hash.new
-
-      first_name = /[:first_name:]/.gen
-      last_name = /[:last_name:]/.gen
-      first_name.gsub!("'",'')
-      last_name.gsub!("'",'')
-
-      $users[name]['first_name'] = first_name
-      $users[name]['last_name'] = last_name
-      email = Autotest.email.split('@')
-      $users[name]['email'] = "%s+%s%s@%s" % [email[0], first_name.downcase, last_name.downcase, email[1]]
-      $users[name]['password'] = Autotest.password
-
-      $users[name]
+      user[:first_name] = /[:first_name:]/.gen
+      user[:last_name] = /[:last_name:]/.gen
+      email_first_name = prepare_for_email_address(user[:first_name])
+      email_last_name = prepare_for_email_address(user[:last_name])
+      user[:email] = Autotest.email.sub('@', "+#{email_last_name}_#{email_last_name}@")
+      user[:password] = Autotest.password
+      user
     end
 
     def get_user(name)
-      if ($users.nil?) or ($users[name].nil?) 
-        raise "<#Autotest::Users> User #{name} doesn't exist." 
-      end
-
-      $users[name]
+      raise "<#Autotest::Users> User '#{name}' doesn't exist." unless @@users[name]
+      @@users[name]
     end
 
     def set_user_data(name, type, data)
-      $users[name][type] = data
+      @@users[name][type] = data
       data
     end
 
     def get_user_data(name, type)
       user = get_user(name)
-      if user[type].nil?
-        raise "<#Autotest::Users> The '#{type}' doesn't exist for '#{name}' user"
-      end
+      raise "<#Autotest::Users> The '#{type}' doesn't exist for '#{name}' user" unless user[type]
       user[type]
     end
 
     def set_current(name)
-      $current ||= Hash.new
-      if ((name != 'anonymous') and (name != 'anonim'))
-        if $users.nil?
-          raise "<#Autotest::Users> You should use create_user method, before set_current method."
-        end
-        $current['first_name'] = $users[name]['first_name']
-        $current['email'] = $users[name]['email']
-        $current['password'] = $users[name]['password']
+      if name != 'anonymous'
+        user = @@users[short_name]
+        raise "<#Autotest::Users> You should use create_user method, before set_current method." if user.nil?
+        @@current = user
       else
-        $current['first_name'] = 'anonymous'
-        $current['email'] = 'anonymous'
-        $current['password'] = 'anonymous'
+        @@current[:first_name] = 'anonymous'
+        @@current[:email] = 'anonymous'
+        @@current[:password] = 'anonymous'
       end
     end
 
     def get_current(type)
-      if ($current.nil?) or ($current[type].nil?)
-        raise "<#Autotest::Users> You doesn't set current user or '#{type}' doesn't exist fot the current user."
+      if (@@current.nil?) or (@@current[type].nil?)
+        raise "<#Autotest::Users> You didn't set current user or '#{type}' doesn't exist for current user."
       end
-      $current[type]
+      @@current[type]
     end
 
     def user_created?(name)
-      ($users and $users[name]).nil? ? false : true
+      @@users[name].nil? ? false : true
     end
     
     def all_users
-      $users
+      @@users
     end
 
+    private
+    def prepare_for_email_address(something)
+      something.gsub(/[^0-9A-Za-z]/, '').downcase
+    end
   end
 end
 
